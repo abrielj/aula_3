52618e3237c90fd52207156ae246d17c7eba579c
abrielj <yakuza.gabiel@gmail.com>
2024-06-15 17:35:58 -0300

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,65 @@
+#VOCÊ É UM DEV E PRECISA CRIAR UM SISTEMA PARA UMA ESCOLA. 
+#SISTEMA DE NOTAS DE ALUNOS QUE MOSTRE COM ESTATISTICA A MODA ,
+#MEDIA, VARIANÇA E DESVIO DE PADRÃO, DAS NOTAS DE ALUNOS DE UM COLÉGIO,
+#ALÉM DE MOSTRAR aMENOR E A  MAIOR NOTA, SEPARE EM FUNÇÕES DIFERENTES
+
+from teste import *
+from empresa2 import *
+from empresa3 import *
+from empresa4 import *
+from empresa5 import *
+
+def empresa1():
+    print('empresa1')
+    ("salarios", notas)
+    ('media:',calcular_media(notas))
+    ('moda:',calcular_moda(notas))
+    ('variancia:',calcular_variancia(notas))
+    ('desvio de padrão:',calcular_desvio_padrao(notas))
+    ('menor salario:', encontrar_menor(notas))
+    ('maior salario:',encontrar_maior(notas))
+                                               
+
+def empresa2():
+    ('empresa2')
+    ("salarios", notas1)
+    ('media:',calcular_media(notas1))
+    ('moda:',calcular_moda(notas1))
+    ('variancia:',calcular_variancia(notas1))
+    ('desvio de padrão:',calcular_desvio_padrao(notas1))
+    ('menor salario:', encontrar_menor(notas1))
+    ('maior salario:',encontrar_maior(notas))
+
+
+def empresa3():
+    ('empresa3')
+    ("salarios:", notas2)
+    ('media:',calcular_media(notas2))
+    ('moda:',calcular_moda(notas2))
+    ('variancia:',calcular_variancia(notas2))
+    ('desvio de padrão:',calcular_desvio_padrao(notas2))
+    ('menor salario:', encontrar_menor(notas2))
+    ('maior salario:',encontrar_maior(notas2))
+
+
+def empresa4():
+    ('empresa4')
+    ("salarios:", notas3)
+    ('media:',calcular_media(notas3))
+    ('moda:',calcular_moda(notas3))
+    ('variancia:',calcular_variancia(notas3))
+    ('desvio de padrão:',calcular_desvio_padrao(notas3))
+    ('menor salario:', encontrar_menor(notas3))
+    ('maior salario:',encontrar_maior(notas3))
+
+
+def empresa5():
+    ('empresa5')
+    ("salarios:", notas)
+    ('media:',calcular_media(notas4))
+    ('moda:',calcular_moda(notas4))
+    ('variancia:',calcular_variancia(notas4))
+    ('desvio de padrão:',calcular_desvio_padrao(notas4))
+    ('menor salario:', encontrar_menor(notas4))
+    ('maior salario:',encontrar_maior(notas4))
+
