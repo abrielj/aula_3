7ec09465ccc8c3d579f6e6cd81614b7ac2b69288
abrielj <yakuza.gabiel@gmail.com>
2024-06-22 09:36:47 -0300

    Update salario.py

--- salario.py
+++ salario.py
@@ -1,65 +1,68 @@
-#VOCÊ É UM DEV E PRECISA CRIAR UM SISTEMA PARA UMA ESCOLA. 
-#SISTEMA DE NOTAS DE ALUNOS QUE MOSTRE COM ESTATISTICA A MODA ,
-#MEDIA, VARIANÇA E DESVIO DE PADRÃO, DAS NOTAS DE ALUNOS DE UM COLÉGIO,
-#ALÉM DE MOSTRAR aMENOR E A  MAIOR NOTA, SEPARE EM FUNÇÕES DIFERENTES
+print('de qual empresa deseja ver as informações?')
+print('''A)empresa1
+B)empresa2
+C)empresa3
+D)empresa4
+E)empresa5''')
+escolha = input()
 
-from teste import *
-from empresa2 import *
-from empresa3 import *
-from empresa4 import *
-from empresa5 import *
-
-def empresa1():
+if escolha == 'a' or 'A':
+    import statistics
+    notas = [1000,6000,1200,8000,1400]
     print('empresa1')
-    ("salarios", notas)
-    ('media:',calcular_media(notas))
-    ('moda:',calcular_moda(notas))
-    ('variancia:',calcular_variancia(notas))
-    ('desvio de padrão:',calcular_desvio_padrao(notas))
-    ('menor salario:', encontrar_menor(notas))
-    ('maior salario:',encontrar_maior(notas))
-                                               
-
-def empresa2():
-    ('empresa2')
-    ("salarios", notas1)
-    ('media:',calcular_media(notas1))
-    ('moda:',calcular_moda(notas1))
-    ('variancia:',calcular_variancia(notas1))
-    ('desvio de padrão:',calcular_desvio_padrao(notas1))
-    ('menor salario:', encontrar_menor(notas1))
-    ('maior salario:',encontrar_maior(notas))
-
-
-def empresa3():
-    ('empresa3')
-    ("salarios:", notas2)
-    ('media:',calcular_media(notas2))
-    ('moda:',calcular_moda(notas2))
-    ('variancia:',calcular_variancia(notas2))
-    ('desvio de padrão:',calcular_desvio_padrao(notas2))
-    ('menor salario:', encontrar_menor(notas2))
-    ('maior salario:',encontrar_maior(notas2))
-
-
-def empresa4():
-    ('empresa4')
-    ("salarios:", notas3)
-    ('media:',calcular_media(notas3))
-    ('moda:',calcular_moda(notas3))
-    ('variancia:',calcular_variancia(notas3))
-    ('desvio de padrão:',calcular_desvio_padrao(notas3))
-    ('menor salario:', encontrar_menor(notas3))
-    ('maior salario:',encontrar_maior(notas3))
-
-
-def empresa5():
-    ('empresa5')
-    ("salarios:", notas)
-    ('media:',calcular_media(notas4))
-    ('moda:',calcular_moda(notas4))
-    ('variancia:',calcular_variancia(notas4))
-    ('desvio de padrão:',calcular_desvio_padrao(notas4))
-    ('menor salario:', encontrar_menor(notas4))
-    ('maior salario:',encontrar_maior(notas4))
-
+    print("salarios", notas)
+    print('media:',sum(notas) / len(notas))
+    print('moda:',statistics.mode(notas))
+    print('variancia:',statistics.variance(notas))
+    print('desvio de padrão:',statistics.stdev(notas))
+    print('menor salario:', min(notas))
+    print('maior salario:',max(notas))
+    
+elif escolha == 'b' or 'B':
+    import statistics
+    notas = [5000,4000,3000,2000,7000]
+    print('empresa2')
+    print("salarios", notas)
+    print('media:',sum(notas) / len(notas))
+    print('moda:',statistics.mode(notas))
+    print('variancia:',statistics.variance(notas))
+    print('desvio de padrão:',statistics.stdev(notas))
+    print('menor salario:', min(notas))
+    print('maior salario:',max(notas))
+    
+elif escolha == 'c' or 'C':
+    import statistics
+    notas = [1200,1300,8000,3000,15000]
+    print('empresa3')
+    print("salarios", notas)
+    print('media:',sum(notas) / len(notas))
+    print('moda:',statistics.mode(notas))
+    print('variancia:',statistics.variance(notas))
+    print('desvio de padrão:',statistics.stdev(notas))
+    print('menor salario:', min(notas))
+    print('maior salario:',max(notas))
+    
+elif escolha == 'd' or 'D':
+    import statistics
+    notas = [1400,1750,2000,4500,5900,7000]
+    print('empresa4')
+    print("salarios", notas)
+    print('media:',sum(notas) / len(notas))
+    print('moda:',statistics.mode(notas))
+    print('variancia:',statistics.variance(notas))
+    print('desvio de padrão:',statistics.stdev(notas))
+    print('menor salario:', min(notas))
+    print('maior salario:',max(notas))
+    
+    
+elif escolha == 'e' or 'E':
+    import statistics
+    notas = [1400,1750,2000,4500,5900,7000]
+    print('empresa5')
+    print("salarios", notas)
+    print('media:',sum(notas) / len(notas))
+    print('moda:',statistics.mode(notas))
+    print('variancia:',statistics.variance(notas))
+    print('desvio de padrão:',statistics.stdev(notas))
+    print('menor salario:', min(notas))
+    print('maior salario:',max(notas))
